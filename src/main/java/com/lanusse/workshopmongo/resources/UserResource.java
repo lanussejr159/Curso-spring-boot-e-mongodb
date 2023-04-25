3e638ca86ec4b1ff86b3931321e2d1aaf5cecf12
lanussejr159 <juniorjf2011166@gmail.com>
2023-04-25 16:26:09 -0300

    Aula 341

--- src/main/java/com/lanusse/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lanusse/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.lanusse.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lanusse.workshopmongo.domain.User;
+import com.lanusse.workshopmongo.dto.UserDTO;
 import com.lanusse.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +22,15 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
-    
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
 }
